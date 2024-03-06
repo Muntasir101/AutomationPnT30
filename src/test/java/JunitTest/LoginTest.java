8028529172a8fdf7a9323b2258ce092b5bd2d463
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-06 10:33:31 +0600

    add assertion

--- src/test/java/JunitTest/LoginTest.java
+++ src/test/java/JunitTest/LoginTest.java
@@ -1,6 +1,7 @@
 package JunitTest;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -51,8 +52,18 @@ public class LoginTest extends BaseTest {
         email.sendKeys(Common.getTextFromFile("src/test/java/TN_Automation/users.txt"));
         password.sendKeys("123456");
         loginButton.click();
+
+        // verification by Logout Text
+        WebElement LogoutLink = driver.findElement(By.cssSelector(".list-group .list-group-item:nth-of-type(13)"));
+        String LogoutText = LogoutLink.getText();
+        Assert.assertEquals("Logout",LogoutText);
+
+        // verification by URL
+        String ExpectedAccountPageUrl = baseUrl +"?route=account/account";
+        String ActualAccountPageUrl = driver.getCurrentUrl();
+        Assert.assertEquals(ExpectedAccountPageUrl,ActualAccountPageUrl);
     }
-    private static void loginTestCase02() throws IOException {
+    private static void loginTestCase02() {
         /*
         This is invalid Test Case.
         Email is Invalid but password valid
