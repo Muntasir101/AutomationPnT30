54cd929f10edc305f75e14704aae71eef8cc7a7c
thisHasan <shantoneel1203@gmail.com>
2024-03-10 11:37:13 +0600

    spaces cleared boss

--- src/test/java/TN_Automation/ProductSearchTest.java
+++ src/test/java/TN_Automation/ProductSearchTest.java
@@ -1,30 +1,20 @@
 package TN_Automation;
-//Question: Any library imported here without a need?
-
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.time.Duration;
 
-
-import static TN_Automation.BaseTest.driver;
-
 public class ProductSearchTest extends BaseTest {
-
     private static final String baseUrl = "https://tutorialsninja.com/demo/";
 
     @Test
     public void explicitWaitTest() throws InterruptedException {
         initializeWebDriver();
         DemoSearch();
-
     }
-
     public static void DemoSearch(){
-
         driver.get(baseUrl); // navigate to  page
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
 
@@ -32,9 +22,5 @@ public class ProductSearchTest extends BaseTest {
         searchBox.sendKeys("macbook");
         WebElement searchButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa-search\n")));
         searchButton.click();
-
-
     }
-
-
 }
