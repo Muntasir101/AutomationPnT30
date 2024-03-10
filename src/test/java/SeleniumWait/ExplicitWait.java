d21a355a2e14813a97337c58a96e211866023463
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-10 11:47:30 +0600

    explicit wait

--- src/test/java/SeleniumWait/ExplicitWait.java
+++ src/test/java/SeleniumWait/ExplicitWait.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.Duration;
-<<<<<<< HEAD
 
-=======
->>>>>>> dee16d86aa9f45edc15e3830de805cd1f5091b30
 
 public class ExplicitWait extends BaseTest {
     private static final String baseUrl = "https://www.shohoz.com/bus-tickets";
