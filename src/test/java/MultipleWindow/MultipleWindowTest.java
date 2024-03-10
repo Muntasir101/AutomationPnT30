ba390c78ebb514614402ccbf981efbfd516f4602
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-10 12:53:29 +0600

    multiple window

---
+++ src/test/java/MultipleWindow/MultipleWindowTest.java
@@ -0,0 +1,45 @@
+package MultipleWindow;
+
+import TN_Automation.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class MultipleWindowTest extends BaseTest {
+    private static final String baseUrl = "https://the-internet.herokuapp.com/windows";
+
+    @Test
+    public void multipleWindowHandle(){
+        initializeWebDriver();
+        switchWindow();
+    }
+    public static void switchWindow(){
+        driver.get(baseUrl);
+
+        // Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+
+        WebElement newWindowButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".example [target]")));
+        newWindowButton.click();
+
+        Set<String> windows = driver.getWindowHandles();
+
+        //switch to child window
+        for(String window : windows ){
+            driver.switchTo().window(window);
+        }
+        driver.get("https://google.com");
+
+        //close new window
+        driver.close();
+
+        // back to parent window
+        driver.switchTo().window(windows.iterator().next());
+
+    }
+}
