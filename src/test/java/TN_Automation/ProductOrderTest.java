b01f934365f21e7576df041b49edaab5efd27334
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-11 12:46:39 +0600

    TN product order done

--- src/test/java/TN_Automation/ProductOrderTest.java
+++ src/test/java/TN_Automation/ProductOrderTest.java
@@ -26,22 +26,65 @@ public class ProductOrderTest extends BaseTest{
         WebElement macBookAddToCartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content [class='product-layout col-lg-3 col-md-3 col-sm-6 col-xs-12']:nth-of-type(1) .hidden-md")));
         macBookAddToCartButton.click();
 
+        String expectedSuccessMessage = "Success: You have added MacBook to your shopping cart!\n" +
+                "×";
+        WebElement verificationMessage =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#common-home > .alert.alert-dismissible.alert-success")));
+        String actualSuccessMessage =  verificationMessage.getText();
+
+        if(expectedSuccessMessage.equals(actualSuccessMessage)){
+            System.out.println("Verification message match.Test Passed.");
+        }
+        else{
+            System.out.println("Verification message not match.Test Failed.Actual Message:"+ actualSuccessMessage);
+        }
+
+
         WebElement cartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-inverse")));
         cartButton.click();
 
         WebElement viewCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a:nth-of-type(1) > strong")));
         viewCart.click();
 
-        String macExpectedUnitPrice = "$602.00";
+        String macExpectedUnitPriceText = "$602.00";
+        // Remove the dollar sign
+        String priceWithoutDollar = macExpectedUnitPriceText.replace("$", "");
+        // Convert to double
+        double macExpectedUnitPrice = Double.parseDouble(priceWithoutDollar);
+
 
         WebElement macUnitPrice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table.table-bordered > tbody > tr > td:nth-of-type(5)")));
-        String macActualUnitPrice =  macUnitPrice.getText();
+        String macActualUnitPriceText =  macUnitPrice.getText();
+        // Remove the dollar sign
+        String macActualUnitPriceWithoutDollar = macActualUnitPriceText.replace("$", "");
+        // Convert to double
+        double macActualUnitPrice = Double.parseDouble(macActualUnitPriceWithoutDollar);
 
-        if(macExpectedUnitPrice.equals(macActualUnitPrice)){
+        if(macActualUnitPrice==macExpectedUnitPrice){
             System.out.println("Unit price match.Test Passed.");
         }
         else{
             System.out.println("Unit price not match.Test Failed.");
+        }
+
+        WebElement macQuantity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-block .form-control")));
+        String macActualQuantity = macQuantity.getAttribute("value");
+        double quantityDouble = Double.parseDouble(macActualQuantity);
+
+
+        double expectedTotalPrice = quantityDouble * macExpectedUnitPrice;
+
+        WebElement macActualTotalPrice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("tbody > tr > td:nth-of-type(6)")));
+        String macFinalTotalPriceText =  macActualTotalPrice.getText();
+        // Remove the dollar sign
+        String macFinalTotalPriceWithoutDollar = macFinalTotalPriceText.replace("$", "");
+        // Convert to double
+        double macFinalTotalPrice = Double.parseDouble(macFinalTotalPriceWithoutDollar);
+
+        if(macFinalTotalPrice==expectedTotalPrice){
+            System.out.println("Total price match.Test Passed.");
+        }
+        else{
+            System.out.println("Total price not match.Test Failed.");
         }
 
     }
