557fe160e0e7568af29cbdea58401cce066595e1
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-11 10:16:52 +0600

    all links

---
+++ src/test/java/EndToEnd/PageAllLinks.java
@@ -0,0 +1,41 @@
+package EndToEnd;
+
+import TN_Automation.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class PageAllLinks extends BaseTest {
+    private static final String baseUrl = "https://the-internet.herokuapp.com/checkboxes";
+
+    @Test
+    public void pageAllLinksTest(){
+        initializeWebDriver();
+        getAllLinks();
+        tearDown();
+    }
+    public static void getAllLinks(){
+        driver.get(baseUrl);
+
+        // find all the links
+        List <WebElement> links = driver.findElements(By.tagName("a"));
+        int linksCount = links.size();
+        System.out.println("Total Number of Links = "+linksCount);
+
+        // open each link in new tab
+        for(WebElement link : links) {
+            String linkUrl = link.getAttribute("href");
+            System.out.println(linkUrl);
+
+            if(linkUrl != null && !linkUrl.isEmpty()){
+                //open link i new tab
+                JavascriptExecutor js = (JavascriptExecutor)driver;
+                js.executeScript("window.open(arguments[0])",linkUrl);
+                System.out.println("Title: "+driver.getTitle());
+            }
+        }
+    }
+}
