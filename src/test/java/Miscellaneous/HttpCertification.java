6ca0185c42243e5cd97e7b63ebfd8a24825b4107
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-13 09:33:46 +0600

    HTTPS certificate

---
+++ src/test/java/Miscellaneous/HttpCertification.java
@@ -0,0 +1,28 @@
+package Miscellaneous;
+
+import TN_Automation.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class HttpCertification extends BaseTest {
+    private static final String baseUrl = "https://www.cacert.org/";
+
+    @Test
+    public void testHttpCertification() {
+        ignoreHttpsCertification();
+    }
+    public static void ignoreHttpsCertification() {
+        // Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.get(baseUrl);
+
+    }
+}
