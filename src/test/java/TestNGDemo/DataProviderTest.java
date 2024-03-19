1433305cfe3bab31a3f40d6a8160235a72c1c809
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-19 10:37:16 +0600

    add log in data provider

--- src/test/java/TestNGDemo/DataProviderTest.java
+++ src/test/java/TestNGDemo/DataProviderTest.java
@@ -7,11 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.*;
-
 import java.io.IOException;
 
-import static TestNGDemo.BaseTest_For_TestNG.driver;
 
 public class DataProviderTest {
     private static final String baseUrl = "https://tutorialsninja.com/demo/index.php";
@@ -23,17 +22,18 @@ public class DataProviderTest {
         if (browserName.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
             driver.manage().window().maximize();
-            System.out.println("Chrome browser is launched");
+            Reporter.log("Chrome browser is launched");
         } else if (browserName.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
-            System.out.println("Firefox browser is launched");
+            Reporter.log("Firefox browser is launched");
         }else if (browserName.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
             driver.manage().window().maximize();
-            System.out.println("EDGE browser is launched");
+            Reporter.log("EDGE browser is launched");
         }
         else{
             System.out.println("Unsupported Browser !!!");
+            Reporter.log("Unsupported Browser !!!");
         }
     }
 
@@ -43,18 +43,23 @@ public class DataProviderTest {
         This is valid Test Case
          */
         driver.get(baseUrl +"?route=account/login"); // navigate to login page
+        Reporter.log("Login Page open");
 
         WebElement email = driver.findElement(By.cssSelector("#input-email"));
         WebElement password = driver.findElement(By.cssSelector("#input-password"));
         WebElement loginButton = driver.findElement(By.cssSelector("input[type=\"submit\"].btn.btn-primary"));
 
         email.sendKeys(dp_email);
+        Reporter.log("Enter Email");
         password.sendKeys(dp_password);
+        Reporter.log("Enter Password");
         loginButton.click();
+        Reporter.log("Login Button Clicked");
 
         // verification by Logout Text
         WebElement LogoutLink = driver.findElement(By.cssSelector(".list-group .list-group-item:nth-of-type(13)"));
         String LogoutText = LogoutLink.getText();
+        Reporter.log("Verification by Logout Text: "+ LogoutText);
         // Assert.assertEquals("Logout",LogoutText);
 
         // verification by URL
@@ -69,9 +74,11 @@ public class DataProviderTest {
         if(driver != null){
             driver.quit();
             System.out.println("Test Complete");
+            Reporter.log("Test Complete");
         }
         else{
             System.out.println("No Driver found.");
+            Reporter.log("No Driver found.");
         }
     }
     @DataProvider(name = "LoginTestData_valid")
